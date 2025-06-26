cd9996321c857077f84149d1c01168609ae6fb22
sudamsiths <sudamsiths.com>
2025-06-26 17:09:44 +0530

    Layered Architecture

---
+++ src/main/java/repository/custom/SupplierRepository.java
@@ -0,0 +1,8 @@
+package repository.custom;
+
+import DTO.Supplier;
+import Entity.SupplierEntity;
+import repository.CRUDRepository;
+
+public interface SupplierRepository extends CRUDRepository<SupplierEntity,String> {
+}
